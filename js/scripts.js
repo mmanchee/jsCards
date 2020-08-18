a891efcd91ecd0667210160be7a873063646d36f
Mike Manchee <mikemanchee@gmail.com>
2020-08-17 20:05:54 -0700

    add css and forEach loops to the project

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,18 @@
 $(document).ready(function() {
   let suits = ["Hearts", "Clubs",  "Diamonds", "Spades"];
-  let values = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
-  $().submit(function() {
-    
+  let values = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
+  let card;
+
+  $("#show-cards").click(function(event) {
+    suits.forEach(function(suit) {
+      values.forEach(function(value) {
+        card = value + " of " + suit;
+        $("#" + suit).append("<li>" + card + "</li>");
+      });
+    });
+
+    $(".playing-cards").show();
+
+    event.preventDefault();
   })
 });
