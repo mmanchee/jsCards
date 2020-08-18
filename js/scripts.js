b3c6e22c3050adf062c21c2a7df371a1b8fb2e7c
Mike Manchee <mikemanchee@gmail.com>
2020-08-17 19:47:20 -0700

    add js arrays to the project

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  let suits = ["Hearts", "Clubs",  "Diamonds", "Spades"];
+  let values = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+  $().submit(function() {
+    
+  })
+});
